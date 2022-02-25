bce17ab296e476bc8cac14b403f13a07cdf42770
Vinh <vinh.bui0101@gmail.com>
2022-02-25 14:44:28 -0800

    Got user's input and stored in local storage

--- script.js
+++ script.js
@@ -1,28 +1,59 @@
+//Data Structures//
+
 let myLibrary = [];
+/*
 function Book(title, author, pages, read){
   this.title = title
   this.author = author
   this.pages = pages
   this.read = read
+}*/
+
+class Book{
+  constructor(title, author, pages, isRead){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = isRead;
+  }
 }
 
 function addBookToLibrary(){
+  //check if user read the book
+  checkTF();
+  //Creating an object from the 'BOOK' class and pushing to our Library array
+  const book = new Book(title.value, author.value, pages.value, isRead.value)
+  myLibrary.push(book);
+  //set 'books' as key, and put myLibrary as value
+  localStorage.setItem('books', JSON.stringify(myLibrary));
+}
 
+//if 'books' data is empty, then myLibrary is empty else when local storage calls for 'books', all of the old book will be store in 'oldBooks' variable and that will become the library
+if(localStorage.getItem('books') == null){
+  myLibrary = [];
+}else{
+  //get the value stored in 'books' and make myLibrary equal to it
+  const oldBooks = JSON.parse(localStorage.getItem('books'));
+  myLibrary = oldBooks;
 }
 
 function showMyLibrary(){
-  //Loops through the array and displays each book on the page
+
 }
 
-const addBook_btn = document.getElementById("addBookBtn");
-const overlay = document.getElementById("overlay");
-const modal = document.getElementById("modal");
+//UI
+
+//Modal//
+const addBook_btn = document.getElementById('addBookBtn');
+const overlay = document.getElementById('overlay');
+const modal = document.getElementById('modal');
 
 addBook_btn.addEventListener('click', () => {
   openModal(modal)
 })
 
 document.addEventListener('click', function(event){
+//if the user clicks outside of the modal, it will automatically close the modal
   if(!event.target.closest(".modal") && !event.target.closest(".addbook")){
     closeModal(modal)
   }
@@ -38,3 +69,26 @@ function closeModal(modal){
   modal.classList.remove('active')
   overlay.classList.remove('active')
 }
+
+
+//Form input//
+const form = document.getElementById('addBookForm');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+const isRead = document.getElementById('isRead');
+
+//make true & false to see if the reader have read the book
+function checkTF(){
+  if(isRead.checked == true){
+    isRead.value = true;
+  }else{
+    isRead.value = false;
+  }
+}
+
+//only call when 'SUBMIT' btn is pressed
+document.addEventListener('DOMContentLoaded', ()=>{
+  document.getElementById('btn').addEventListener('click', addBookToLibrary);
+});
+
